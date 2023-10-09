f540b206e5f0f39c92cabfe75b547381aef80c14
adham77777 <adhamhany781@gmail.com>
2023-10-08 18:19:45 -0700

    m

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,9 @@ nclude <stdlib.h>
  *      */
 void *malloc_checked(unsigned int b)
 {
-		void *ptr;
-
-			ptr = malloc(b);
-
-				if (ptr == NULL)
-							exit(98);
-
-					return (ptr);
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
-
